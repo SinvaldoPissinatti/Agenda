e22761ea68fc0fd84b16b01de55a91f5cbbc6a11
conta <sinvaldopissinatti2@gmail.com>
2023-03-28 18:05:21 -0300

    Incuído a fun~çao e tela de autenticação

--- core/views.py
+++ core/views.py
@@ -1,16 +1,39 @@
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 # def index(request):
 #     return redirect('/agenda/')
-def local_eventos(request, titulo_evento):
-    evento = Evento.objects.get(titulo=titulo_evento)
-    mensagem = '<h1>O local do {} é: {}</h1>'.format(titulo_evento, evento.local)
-    return HttpResponse (mensagem)
+# def local_eventos(request, titulo_evento):
+#     evento = Evento.objects.get(titulo=titulo_evento)
+#     mensagem = '<h1>O local do {} é: {}</h1>'.format(titulo_evento, evento.local)
+#     return HttpResponse (mensagem)
+def login_user(request):
+    return render(request, 'login.html')
 
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def autenticacao_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválido")
+
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    usuario = request.user
-    eventos = Evento.objects.all()
+    user = request.user
+    eventos = Evento.objects.filter(usuario=user)
     dados = {'eventos' : eventos}
     return render(request, 'agenda.html', dados)
