5ffd3a92df3667a8693b0f1f2db2d42ecb6280f2
conta <sinvaldopissinatti2@gmail.com>
2023-03-28 11:25:33 -0300

    Adicionei uma coluna somente para a informação do local da atividade, depois criei uma view e um url que faz a requisição do local a partir do nome do evento cadastrado

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 # Create your views here.
+def local_eventos(request, titulo_evento):
+    evento = Evento.objects.get(titulo=titulo_evento)
+    mensagem = '<h1>O local do {} é: {}</h1>'.format(titulo_evento, evento.local)
+    return HttpResponse (mensagem)
