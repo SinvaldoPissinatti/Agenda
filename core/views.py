85e04a6d0eb414d2b1b056777732a0761c4bbfc1
conta <sinvaldopissinatti2@gmail.com>
2023-03-28 15:19:41 -0300

    Cirado tela de listagem de eventos

--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
 # Create your views here.
+
+# def index(request):
+#     return redirect('/agenda/')
 def local_eventos(request, titulo_evento):
     evento = Evento.objects.get(titulo=titulo_evento)
     mensagem = '<h1>O local do {} é: {}</h1>'.format(titulo_evento, evento.local)
     return HttpResponse (mensagem)
+
+def lista_eventos(request):
+    usuario = request.user
+    eventos = Evento.objects.all()
+    dados = {'eventos' : eventos}
+    return render(request, 'agenda.html', dados)
